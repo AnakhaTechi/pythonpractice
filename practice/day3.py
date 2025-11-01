bc8d4e3b455542cbf7d475f746468b2ed1981f18
AnakhaTechi <anakhatj04@gmail.com>
2025-11-01 21:13:45 +0530

    Add Day 1 notes to day3.py

---
+++ practice/day3.py
@@ -0,0 +1,10 @@
+# Day 1 Notes 📝
+
+### What I learned:
+- What GitHub is
+- How to create a repository
+- What README.md is
+- How to add files and commit changes
+
+### Feelings:
+Excited to start learning and building consistently 🚀
